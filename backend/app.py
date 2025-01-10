a0f4f5c2d7f0c25bd2474a0752dafc069c302b59
Matan Haimov <shlomihaimov1@gmail.com>
2025-01-10 15:04:08 +0200

    integrate and setup ai advisor

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,19 @@
 from flask import Flask
-from routes.auth import auth_bp
+import os
+from dotenv import load_dotenv
 
+# Routes
+from routes.auth import auth_bp
+from routes.ai_advior import ai_advior_bp
+
+
+load_dotenv()
 app = Flask(__name__)
 
 # Routes
 app.register_blueprint(auth_bp, url_prefix='/auth')
-
+app.register_blueprint(ai_advior_bp, url_prefix='/ai_advior')
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.getenv("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
