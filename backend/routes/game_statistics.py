5a29c81ac832b640d3e3b4ad6bac4eb9d0ca4d35
noamgloberman0 <noam.globerman@placer.ai>
2025-01-11 15:08:49 +0200

    Add endpoint for get statistics by id

--- backend/routes/game_statistics.py
+++ backend/routes/game_statistics.py
@@ -1,8 +1,13 @@
 from flask import Blueprint, request
-from controllers.game_statistics import create_game_statistics
+from controllers.game_statistics import create_game_statistics, get_game_statistics_by_id
 
 game_statistics_bp = Blueprint('game_statistics', __name__)
 
 @game_statistics_bp.route('/create', methods=['POST'])
 def create():
     return create_game_statistics(request)
+
+
+@game_statistics_bp.route('/game_id/<game_id>', methods=['GET'])
+def get_game_statistics_by_id_route(game_id):
+    return get_game_statistics_by_id(game_id)
