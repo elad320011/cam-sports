ccd28402a70501b0d6ea4917a5a94942a23861fc
Thedin87 <daniel.nehmadi@gmail.com>
2025-04-01 00:30:13 +0300

    triggering ci now works

--- backend/routes/ci.py
+++ backend/routes/ci.py
@@ -1,8 +1,8 @@
 from flask import Blueprint, request
 from controllers.ci import trigger_ci
 
-ci_bp = Blueprint('training_plans', __name__)
+ci_bp = Blueprint('ci_bp', __name__)
 
 @ci_bp.route('/trigger', methods=['POST'])
-def trigger_ci():
+def trigger_ci_route():
     return trigger_ci(request)
